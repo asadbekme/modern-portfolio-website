320ef464554bf60ae03b6f193f2a689544c9e084
asadbekme <asadbekme2002@gmail.com>
2026-01-14 12:30:50 +0500

    fix: fixed build error

--- app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-// app/not-found.tsx (root level)
-// This redirects to the localized not-found page
-import { redirect } from "next/navigation";
-
-export default function RootNotFound() {
-  // Redirect to default locale's not-found
-  redirect("/en");
-}
